faa2589611116e966448c5bd309fdcbda258c584
bozya02 <bozinandrey1@yandex.ru>
2023-01-11 13:40:43 +0300

    Add project files.

---
+++ Core/DataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataBase;
+
+namespace Core
+{
+    public class DataAccess
+    {
+        public static List<Tour> GetTours() => VediGroupEntities.GetContext().Tours.ToList();
+    }
+}
---
+++ Core/DataBase/VediGroupEntities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataBase
+{
+    public partial class VediGroupEntities
+    {
+        private static VediGroupEntities context;
+        public static VediGroupEntities GetContext()
+        {
+            if (context == null)
+                context = new VediGroupEntities();
+            return context;
+        }
+    }
+}
