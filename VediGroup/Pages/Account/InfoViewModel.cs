4993f08d560802c1b7945d222777040a2a667d12
Мишка <gutufik@mail.ru>
2023-01-13 14:06:23 +0300

    Add user info editing

---
+++ VediGroup/Pages/Account/InfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.DataBase;
+
+namespace VediGroup.Pages.Account
+{
+    public class InfoViewModel
+    {
+        public User User { get; set; }
+        public InfoViewModel()
+        {
+            User = new User();
+        }
+
+    }
+}
