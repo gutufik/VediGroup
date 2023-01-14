e2f4e39c4e269b6dc31446f9b64818fbf3953230
Мишка <gutufik@mail.ru>
2023-01-14 12:30:12 +0300

    Add tor page template

---
+++ VediGroup/Pages/ToursPages/TourModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace VediGroup.Pages.ToursPages
+{
+    public class TourModel : ComponentBase 
+    {
+        public TourModel()
+        {
+            ViewModel= new TourViewModel();
+        }
+
+        public TourViewModel ViewModel { get; set; }
+
+        public async Task SaveAsync()
+        {
+
+        }
+
+    }
+}
---
+++ VediGroup/Pages/ToursPages/TourViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataBase;
+
+namespace VediGroup.Pages.ToursPages
+{
+    public class TourViewModel
+    {
+        public Tour Tour { get; set; }
+        public TourViewModel() 
+        {
+            Tour = new Tour();
+        }
+    }
+}
