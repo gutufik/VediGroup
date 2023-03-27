93e2204bfd3f1995c8fc3652b701660aa6c001ba
Salavat <abrarov03@gmail.com>
2023-03-27 19:59:13 +0300

    Started HotelsPages

---
+++ VediGroup/Pages/HotelsPages/HotelViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Core.DataBase;
+using System.ComponentModel.DataAnnotations;
+
+namespace VediGroup.Pages.HotelsPages
+{
+    public class HotelViewModel
+    {
+        private string name;
+
+        public HotelViewModel()
+        {
+
+            Hotel = new Hotel();
+        }
+        public Hotel Hotel { get; set; }
+
+        [Required]
+        public string Name { get => Hotel.Name; set => Hotel.Name = value; }
+
+        public int CityId { get => Hotel.CityId; set { Hotel.CityId = value; } }
+
+    }
+
+
+}
